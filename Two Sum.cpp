1e27f803fb17a85dc24340edafdc6e2098d79215
Prithvijit Basak <68730583+prithvijitbasak@users.noreply.github.com>
2021-11-01 10:38:34 +0530

    Create Two Sum.cpp

---
+++ Two Sum.cpp
@@ -0,0 +1,21 @@
+//Editorial link: https://leetcode.com/problems/two-sum/discuss/674/C%2B%2B-one-pass
+//Code:
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        if(nums.size()==0)
+            return ans;
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(mp.find(target-nums[i])!=mp.end())
+            {
+                ans.push_back(mp[target-nums[i]]);
+                ans.push_back(i);
+            }
+            mp[nums[i]]=i;
+        }
+        return ans;
+    }
+};
