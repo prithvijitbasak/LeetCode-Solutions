3cc33c1951e18515103dc1f975041e961df9ec1a
Prithvijit Basak <68730583+prithvijitbasak@users.noreply.github.com>
2021-11-01 19:15:52 +0530

    Create Remove Element.cpp

---
+++ Remove Element.cpp
@@ -0,0 +1,16 @@
+Editorial: https://leetcode.com/problems/remove-element/discuss/1048331/C-C%2B%2B.-0-ms-faster-than-100.00.-cool-clear-and-easy-solution.
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int len=nums.size();
+        int idx=0;
+        while(idx<len)
+        {
+            if(nums[idx]==val)
+                nums[idx]=nums[len-1],len--;
+            else
+                idx++;
+        }
+        return len;
+    }
+};
