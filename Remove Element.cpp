a30370e1c6fec93520dc5e768989afcc489416cb
Prithvijit Basak <68730583+prithvijitbasak@users.noreply.github.com>
2021-11-01 19:16:14 +0530

    Update Remove Element.cpp

--- Remove Element.cpp
+++ Remove Element.cpp
@@ -1,4 +1,4 @@
-Editorial: https://leetcode.com/problems/remove-element/discuss/1048331/C-C%2B%2B.-0-ms-faster-than-100.00.-cool-clear-and-easy-solution.
+//Editorial: https://leetcode.com/problems/remove-element/discuss/1048331/C-C%2B%2B.-0-ms-faster-than-100.00.-cool-clear-and-easy-solution.
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
