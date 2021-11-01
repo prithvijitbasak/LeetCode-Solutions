8f160a52757f7bf9d73f16fa66ba9294077c883a
Prithvijit Basak <68730583+prithvijitbasak@users.noreply.github.com>
2021-11-01 11:15:00 +0530

    Create Implement strStr().cpp

---
+++ Implement strStr().cpp
@@ -0,0 +1,54 @@
+// Editorial: This problem can be solved using KMP algorithm.
+// To know about KMP algorithm watch: https://youtu.be/4jY57Ehc14Y
+// But still if have problem this is the perfect editorial: https://leetcode.com/problems/implement-strstr/discuss/12956/C%2B%2B-Brute-Force-and-KMP
+class Solution {
+public:
+	int strStr(string haystack, string needle) {
+        int n=needle.size();
+        if(n==0)
+        {https://leetcode.com/problems/implement-strstr/discuss/
+            return 0;
+        }
+		vector<int> lps(n);
+        lpsarray(haystack,needle,lps);
+        int i=0,j=0;
+        int m=haystack.size();
+        while(i<m)
+        {
+            if(haystack[i]==needle[j])
+            {
+                i++;
+                j++;
+            }
+            if(j==n)
+                return i-j;
+            if(haystack[i]!=needle[j] and i<m)
+            {
+                if(j!=0)
+                    j=lps[j-1];
+                else
+                    i++;
+            }
+        }
+        return -1;
+	}
+    void lpsarray(string haystack,string needle,vector<int> &lps)
+    {
+        int len=0,i=1;
+        int m=needle.size();
+        lps[0]=0;
+        while(i<m)
+        {
+            if(needle[i]==needle[len])
+            {
+                len++;
+                lps[i]=len;
+                i++;
+            }
+            else if(len)
+                len=lps[len-1];
+            else
+                lps[i++]=0;
+        }
+    }
+};
