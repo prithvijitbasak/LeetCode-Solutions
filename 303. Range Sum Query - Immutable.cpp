5a25ea42676c0483814c99b3081bfda95b57c913
Prithvijit Basak <68730583+prithvijitbasak@users.noreply.github.com>
2022-01-11 14:09:44 +0530

    Update 303. Range Sum Query - Immutable.cpp

--- 303. Range Sum Query - Immutable.cpp
+++ 303. Range Sum Query - Immutable.cpp
@@ -1,4 +1,43 @@
 //Editorial: Same as (LeetCode problem)307. Range Sum Query - Mutable
+// Using Segment tree
+class NumArray {
+public:
+    vector<int> seg,arr;
+    int n;
+    void build(int idx,int left,int right)
+    {
+        if(left==right)
+        {
+            seg[idx]=arr[left];
+            return;
+        }
+        int mid=(left+right)/2;
+        build(2*idx+1,left,mid);
+        build(2*idx+2,mid+1,right);
+        seg[idx]=seg[2*idx+1]+seg[2*idx+2];
+    }
+    int rangesum(int pos,int low,int high,int left,int right)
+    {
+        if(left<=low and right>=high)
+            return seg[pos];
+        if(low>right or high<left)
+            return 0;
+        int mid=(low+high)/2;
+        return rangesum(2*pos+1,low,mid,left,right)+rangesum(2*pos+2,mid+1,high,left,right);
+    }
+    NumArray(vector<int>& nums) {
+        arr=nums;
+        n=nums.size();
+        seg.resize(4*n,0);
+        build(0,0,n-1);
+    }
+    
+    int sumRange(int left, int right) {
+        return rangesum(0,0,n-1,left,right);
+    }
+};
+
+// Using Fenwick tree
 class NumArray {
 public:
     vector<int> bit,arr;
