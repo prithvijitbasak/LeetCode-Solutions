8317494ddac9eb83e9321d9f1795430f17f945ae
Prithvijit Basak <68730583+prithvijitbasak@users.noreply.github.com>
2022-01-16 08:54:22 +0530

    Create 12. Integer to Roman.cpp

---
+++ 12. Integer to Roman.cpp
@@ -0,0 +1,11 @@
+//Editorial: https://leetcode.com/problems/integer-to-roman/discuss/6274/Simple-Solution
+class Solution {
+public:
+    string intToRoman(int num) {
+        string M[]={"", "M", "MM", "MMM"};
+        string C[]={"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        string X[]={"","X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        string I[]={"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        return M[num/1000]+C[(num%1000)/100]+X[(num%100)/10]+I[num%10];
+    }
+};
