ae9eee809cb62cf02bb92484a651d9250719f688
Prithvijit Basak <68730583+prithvijitbasak@users.noreply.github.com>
2022-02-01 11:10:37 +0530

    Create 46. Permutations.cpp

---
+++ 46. Permutations.cpp
@@ -0,0 +1,23 @@
+//Editorial: https://youtu.be/f2ic2Rsc9pU
+class Solution {
+public:
+    void solve(int idx,vector<int> &nums,vector<vector<int>> &ans)
+    {
+        if(idx==nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=idx;i<nums.size();i++)
+        {
+            swap(nums[idx],nums[i]);
+            solve(idx+1,nums,ans);
+            swap(nums[idx],nums[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        solve(0,nums,ans);
+        return ans;
+    }
+};
