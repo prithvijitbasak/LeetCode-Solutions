1733be31fbee31419990224fc1047b4b6774dde7
Prithvijit Basak <68730583+prithvijitbasak@users.noreply.github.com>
2022-02-03 20:41:43 +0530

    Create 51. N-Queens.cpp

---
+++ 51. N-Queens.cpp
@@ -0,0 +1,37 @@
+//Editorial: https://youtu.be/i05Ju7AftcM
+class Solution {
+public:
+    void solve(int col,vector<string> &board,vector<vector<string>> &ans,vector<int> &leftrow,vector<int> &upperdiagonal, vector<int> &lowerdiagonal,int n)
+    {
+        if(col==n)
+        {
+            ans.push_back(board);
+            return;
+        }
+        for(int row=0;row<n;row++)
+        {
+            if(leftrow[row]==0 and upperdiagonal[n-1+col-row]==0 and lowerdiagonal[row+col]==0)
+            {
+                board[row][col]='Q';
+                upperdiagonal[n-1+col-row]=1;
+                lowerdiagonal[row+col]=1;
+                leftrow[row]=1;
+                solve(col+1,board,ans,leftrow,upperdiagonal,lowerdiagonal,n);
+                board[row][col]='.';
+                upperdiagonal[n-1+col-row]=0;
+                lowerdiagonal[row+col]=0;
+                leftrow[row]=0;
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++)
+            board[i]=s;
+        vector<int> leftrow(n,0),upperdiagonal(2*n-1,0),lowerdiagonal(2*n-1,0);
+        solve(0,board,ans,leftrow,upperdiagonal,lowerdiagonal,n);
+        return ans;
+    }
+};
