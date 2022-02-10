667d97184bd0b43c0f3f8d0852b187a483d69031
Prithvijit Basak <68730583+prithvijitbasak@users.noreply.github.com>
2022-02-10 20:07:46 +0530

    Create 704. Binary Search.cpp

---
+++ 704. Binary Search.cpp
@@ -0,0 +1,18 @@
+//Editorial: Basic binary search is needed to understand.
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low=0,high=nums.size()-1;
+        while(low<=high)
+        {
+            int mid=(low+high)>>1;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]<target)
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+        return -1;
+    }
+};
