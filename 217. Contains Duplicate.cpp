abb06f7c15a8f4a43cbc02d963be0755026ce9a0
Prithvijit Basak <68730583+prithvijitbasak@users.noreply.github.com>
2022-02-11 17:09:07 +0530

    Create 217. Contains Duplicate.cpp

---
+++ 217. Contains Duplicate.cpp
@@ -0,0 +1,13 @@
+//Editorial: Just sort and check for duplicate ones.
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i]==nums[i-1])
+                return true;
+        }
+        return false;
+    }
+};
