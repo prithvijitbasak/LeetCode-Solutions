48385d1f344f622e4f7fd27f8876598a7b5395ed
Prithvijit Basak <68730583+prithvijitbasak@users.noreply.github.com>
2022-03-04 19:38:46 +0530

    Update 503. Next Greater Element II.cpp

--- 503. Next Greater Element II.cpp
+++ 503. Next Greater Element II.cpp
@@ -3,7 +3,7 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n=nums.size();
-        vector<int> nge(n,-1);;
+        vector<int> nge(n,-1);
         stack<int> stk;
         for(int i=2*n-1;i>=0;i--)
         {
