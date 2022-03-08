f97fa1b3958b1cd8f0c228af93cf0612e37a425c
Prithvijit Basak <68730583+prithvijitbasak@users.noreply.github.com>
2022-03-08 14:07:26 +0530

    Update 547. Number of Provinces.cpp

--- 547. Number of Provinces.cpp
+++ 547. Number of Provinces.cpp
@@ -1,3 +1,34 @@
+//Approach 1: Using dfs and without using any visited array for that
+//Editorial: https://leetcode.com/problems/number-of-provinces/discuss/376612/Easy-C++-DFS-Solution/1038941
+class Solution {
+public:
+    void dfs(vector<vector<int>> &grid,int i,int n)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(grid[i][j])
+            {
+                grid[i][j]=0;
+                grid[j][i]=0;
+                dfs(grid,j,n);
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            if(grid[i][i])
+            {
+                dfs(grid,i,n);
+                ans+=1;
+            }
+        }
+        return ans;
+    }
+};
+//Approach 2: Using dsu
 //Editorial: https://leetcode.com/problems/number-of-provinces/discuss/873741/Simple-C%2B%2B-Solution-oror-Fully-Explained-at-begineers-level-oror-Easy-Understanding-of-UNION-concept
 class Solution {
 public:
