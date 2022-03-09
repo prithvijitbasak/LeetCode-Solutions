5d47f82f69fabcca27a78b29e8fce233f0c08e49
Prithvijit Basak <68730583+prithvijitbasak@users.noreply.github.com>
2022-03-09 09:03:45 +0530

    Update 953. Verifying an Alien Dictionary.cpp

--- 953. Verifying an Alien Dictionary.cpp
+++ 953. Verifying an Alien Dictionary.cpp
@@ -1,27 +1,15 @@
 //Editorial: https://leetcode.com/problems/verifying-an-alien-dictionary/discuss/1149842/Simple-Solution-using-HashMap-w-Explanation-or-O(N)-O(1)-or-Beats-100
 class Solution {
 public:
-    unordered_map<char,int> mp;
-    bool checkorder(string a,string b)
-    {
-        int i=0;
-        while(i++<a.size() and i<b.size())
-        {
-            if(mp[a[i]]!=mp[b[i]])
-                return mp[a[i]]<mp[b[i]];
-        }
-        return size(a)<=size(b);
-    }
+    unordered_map<char,char> ump;
     bool isAlienSorted(vector<string>& words, string order) {
-        for(int i=0;i<order.size();i++)
+        for(int i=0;i<size(order);i++)
+            ump[order[i]]=i+'a';
+        for(auto &it:words)
         {
-            mp[order[i]]=i;
+            for(char &ch:it)
+                ch=ump[ch];
         }
-        for(int i=0;i<words.size()-1;i++)
-        {
-            if(!checkorder(words[i],words[i+1]))
-                return false;
-        }
-        return true;
+        return is_sorted(words.begin(),words.end());
     }
 };
