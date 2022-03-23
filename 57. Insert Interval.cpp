42858a0d0d8aecccef9d6e9c0d9c82fb0856d207
Prithvijit Basak <68730583+prithvijitbasak@users.noreply.github.com>
2022-03-23 11:07:04 +0530

    Update 57. Insert Interval.cpp

--- 57. Insert Interval.cpp
+++ 57. Insert Interval.cpp
@@ -16,3 +16,23 @@ public:
         return ans;
     }
 };
+//Approach 2: We can optimise it by changing the implementation method. TC: O(n) SC: O(1)
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> ans;
+        int idx=0;
+        while(idx<intervals.size() and intervals[idx][1]<newInterval[0])
+            ans.push_back(intervals[idx++]);
+        while(idx<intervals.size() and intervals[idx][0]<=newInterval[1])
+        {
+            newInterval[0]=min(newInterval[0],intervals[idx][0]);
+            newInterval[1]=max(newInterval[1],intervals[idx][1]);
+            idx+=1;
+        }
+        ans.push_back(newInterval);
+        while(idx<intervals.size())
+            ans.push_back(intervals[idx++]);
+        return ans;
+    }
+};
