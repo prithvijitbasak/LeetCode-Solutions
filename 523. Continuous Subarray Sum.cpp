32afa15eaa602d253ae6f2e20d3fc00d289ab36f
Prithvijit Basak <68730583+prithvijitbasak@users.noreply.github.com>
2022-07-18 14:30:49 +0530

    Create 523. Continuous Subarray Sum.cpp

---
+++ 523. Continuous Subarray Sum.cpp
@@ -0,0 +1,23 @@
+//Editorial: https://leetcode.com/problems/continuous-subarray-sum/discuss/679929/C++-Code-With-Comments
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> ump;
+        int sum=0,n=nums.size();
+        ump[0]=-1;
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];
+            if(k)
+                sum%=k;
+            if(ump.find(sum)!=ump.end())
+            {
+                if(i-ump[sum]>1)
+                    return true;
+            }
+            else
+                ump[sum]=i;
+        }
+        return false;
+    }
+};
