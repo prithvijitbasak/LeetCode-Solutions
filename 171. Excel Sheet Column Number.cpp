961796b8bc3cdac75aadcfbdbac1a91fbbc97663
Prithvijit Basak <68730583+prithvijitbasak@users.noreply.github.com>
2023-03-02 19:39:48 +0530

    Update 171. Excel Sheet Column Number.cpp

--- 171. Excel Sheet Column Number.cpp
+++ 171. Excel Sheet Column Number.cpp
@@ -1,18 +1,17 @@
-//Editorial: https://leetcode.com/problems/excel-sheet-column-number/discuss/52154/Concise-java-solution-with-explanation.
+//Editorial 1: https://leetcode.com/problems/excel-sheet-column-number/discuss/52154/Concise-java-solution-with-explanation.
+//Editorial 2: https://leetcode.com/problems/excel-sheet-column-title/solutions/441430/detailed-explanation-here-s-why-we-need-n-at-first-of-every-loop-java-python-c/?orderBy=most_votes
 class Solution {
 public:
     int titleToNumber(string s) {
-        // Suppose the test case is "BCM"
-        // then the solution is 
-        //     "B" = 2
-        //     "BC" = (2)26 + 3
-        //     "BCM" = (2(26) + 3)26 + 13
+        //for String ABZ and its corresponding number n:
+        //n = (A+1) * 26^2 + (B+1) * 26^1 + (Z+1) * 26^0
         int n=s.size();
+        int j=n-1;
         int ans=0;
         for(int i=0;i<n;i++)
         {
-            ans*=26;
-            ans+=s[i]-64;
+            ans+=(s[i]-'A'+1)*pow(26,j);
+            j-=1;
         }
         return ans;
     }
