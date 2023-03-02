7ee9f0cc4d4e25bf49947368cf85beb496795e5f
Prithvijit Basak <68730583+prithvijitbasak@users.noreply.github.com>
2023-03-02 14:02:39 +0530

    Update 137. Single Number II.cpp

--- 137. Single Number II.cpp
+++ 137. Single Number II.cpp
@@ -12,3 +12,30 @@ public:
         return ones;
     }
 };
+//Approach 2: Using conventional technique.
+//Editorial: https://leetcode.com/problems/single-number-ii/solutions/43313/a-general-c-solution-for-these-type-problems/?orderBy=most_votes
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        vector<int> bits(32,0);
+        for(auto it:nums)
+        {
+            int val=it;
+            for(int i=0;i<32;i++)
+            {
+                bits[i]+=(val&1);
+                val>>=1;
+                if(!val)
+                    break;
+            }
+        }
+        int ans=0;
+        for(int i=0;i<32;i++)
+        {
+            int rem=bits[i]%3;
+            if(rem)
+                ans+=(1<<i);
+        }
+        return ans;
+    }
+};
