3159b0065d848e54ce6b59add288a32297e4661a
Prithvijit Basak <68730583+prithvijitbasak@users.noreply.github.com>
2023-03-31 18:52:05 +0530

    Update 87. Scramble String.cpp

--- 87. Scramble String.cpp
+++ 87. Scramble String.cpp
@@ -1,34 +1,31 @@
 //Editorial: https://youtu.be/VyHEglhbm-A
 class Solution {
 public:
-    unordered_map<string,bool> mp;
-    bool isScramble(string a, string b) {
-        mp.clear();
-    if(a.size()!=b.size())
-      return 0;
-    return solve(a,b);
-        
-    }
+    unordered_map<string,int> ump;
     bool solve(string a,string b)
-{
-    string key=a+" "+b;
-    if(mp.find(key)!=mp.end())
-      return mp[key];
-    if(a.compare(b)==0)
-      return mp[key]=1;
-    if(a.size()<=1)
-      return mp[key]=0;
-    int flag=0;
-    int n=a.size();
-    for(int i=1;i<n;i++)
     {
-if((solve(a.substr(0,i),b.substr(n-i,i)) and solve(a.substr(i),b.substr(0,n-i))) or (solve(a.substr(0,i),b.substr(0,i)) and solve(a.substr(i),b.substr(i))))
+        string key=a+" "+b;
+        if(ump.find(key)!=ump.end())
+            return ump[key];
+        if(a.compare(b)==0)
+            return ump[key]=1;
+        if(a.size()<1)
+            return ump[key]=0;
+        int n=a.size();
+        int flag=0;
+        for(int i=1;i<n;i++)
         {
-          flag=1;
-          break;
+            if((solve(a.substr(0,i),b.substr(n-i)) and solve(a.substr(i),b.substr(0,n-i))) or (solve(a.substr(0,i),b.substr(0,i)) and solve(a.substr(i),b.substr(i))))
+            {
+                flag=1;
+                break;
+            }
         }
+        return ump[key]=flag;
     }
-    return mp[key]=flag;
-}
-
+    bool isScramble(string a, string b) {
+        if(a.size()!=b.size())
+            return false;
+        return solve(a,b);
+    }
 };
