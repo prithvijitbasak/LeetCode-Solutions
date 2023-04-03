257ebe7e0f00a31bee8dd95d1203e7d92d76cb41
Prithvijit Basak <68730583+prithvijitbasak@users.noreply.github.com>
2023-04-03 07:45:36 +0530

    Update 881. Boats to Save People.cpp

--- 881. Boats to Save People.cpp
+++ 881. Boats to Save People.cpp
@@ -1,15 +1,24 @@
-//Editorial: https://leetcode.com/problems/boats-to-save-people/solution/
+//Editorial: https://leetcode.com/problems/boats-to-save-people/solution/ , https://leetcode.com/problems/boats-to-save-people/solutions/1877945/java-c-a-very-easy-explanation-trust-me/?orderBy=most_votes
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
         sort(people.begin(),people.end());
-        int i=0,j=people.size()-1,ans=0;
+        int i=0,j=people.size()-1;
+        int ans=0;
         while(i<=j)
         {
-            ans+=1;
-            if(people[i]+people[j]<=limit)
-                i++;
-            j--;
+            int sum=people[i]+people[j];
+            if(sum<=limit)
+            {
+                ans+=1;
+                i+=1;
+                j-=1;
+            }
+            else
+            {
+                ans+=1;
+                j-=1;
+            }
         }
         return ans;
     }
