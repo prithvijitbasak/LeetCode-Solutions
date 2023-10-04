76db6c2578bf4d680cf4663e1a57f0f4851d55c2
Prithvijit Basak <68730583+prithvijitbasak@users.noreply.github.com>
2023-10-04 06:26:12 +0530

    Update 34. Find First and Last Position of Element in Sorted Array.cpp

--- 34. Find First and Last Position of Element in Sorted Array.cpp
+++ 34. Find First and Last Position of Element in Sorted Array.cpp
@@ -1,38 +1,52 @@
-//Editorial: https://youtu.be/4sQL7R5ySUU
+//Editorial: In the solution.
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int n=nums.size();
-        int low=0,high=n-1;
         vector<int> ans(2,-1);
-        // for the first occurence 
-        while(low<=high)
+        int n = nums.size();
+        int l = 0, r = n - 1;
+        // for finding the first occurence of the target
+        while(l <= r)
         {
-            int mid=(low+high)>>1;
-            if(nums[mid]==target)
+            // the intuition is the every element in the array is 
+            // occuring either once or more than once
+            // if we need to find the first occurence then 
+            // we will just find by doing binary searching and 
+            // if we find the mid element as the target element 
+            // and then we will just reduce the right search space by doing
+            // r = mid - 1;
+            // because we know that the elements in the right of the mid will always 
+            // be the target as it is non-decreasing and will store it 
+            // in the answer variable
+            int mid = l + (r - l) / 2;
+            if(nums[mid] == target)
             {
-                ans[0]=mid;
-                high=mid-1;
+                ans[0] = mid;
+                r = mid - 1;
             }
-            else if(nums[mid]<target)
-                low=mid+1;
+            else if(nums[mid] < target)
+                l = mid + 1;
             else
-                high=mid-1;
+                r = mid - 1;
         }
-        low=0,high=n-1;
-        // second occurence of the target
-        while(low<=high)
+        l = 0;
+        r = n - 1;
+        // for finding the last occurence of the target
+        while(l <= r)
         {
-            int mid=(low+high)>>1;
-            if(nums[mid]==target)
+            int mid = l + (r - l) / 2;
+            // same as the above but here we are doing 
+            // making the left search space to l = mid - 1;
+            // the concept is same as above
+            if(nums[mid] == target)
             {
-                ans[1]=mid;
-                low=mid+1;
+                ans[1] = mid;
+                l = mid + 1;
             }
-            else if(nums[mid]<target)
-                low=mid+1;
+            else if(nums[mid] < target)
+                l = mid + 1;
             else
-                high=mid-1;
+                r = mid - 1;
         }
         return ans;
     }
